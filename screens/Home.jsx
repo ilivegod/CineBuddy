96bf7724539128fa3b21d54057b323d43c8558bf
ilivegod <sekajunior2014@gmail.com>
2024-04-27 13:51:56 +0000

    objectives sorted

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,25 +1,69 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Feather } from "@expo/vector-icons";
+import { useState } from "react";
 
 const Home = ({ navigation }) => {
+  const [trending, setTrending] = useState([1, 2, 3, 4]);
+
   return (
-    <SafeAreaView>
-      <View style={{ paddingHorizontal: 15 }}>
+    // backgroundColor: "#111010"
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#111010" }}>
+      <View style={{ paddingHorizontal: 20 }}>
         <View
           style={{
             flexDirection: "row",
             justifyContent: "space-between",
           }}
         >
-          <Text>Home</Text>
-          <Text>CineBuddy</Text>
-          <Text>Menu</Text>
+          <TouchableOpacity>
+            <Image
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+                borderWidth: 1,
+                borderColor: "gray",
+              }}
+              source={require("../assets/profile.jpg")}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              width: 40,
+              height: 40,
+              borderRadius: 20,
+              backgroundColor: "gray",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Feather name="search" size={20} color="white" />
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity onPress={() => navigation.navigate("MovieDetail")}>
-          <Text>navigate</Text>
-        </TouchableOpacity>
+        <View style={{ marginTop: 30 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text style={{ color: "white", fontWeight: "600", fontSize: 25 }}>
+              Trending Now
+            </Text>
+
+            <TouchableOpacity>
+              <Text style={{ color: "gray" }}>See all</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <Text style={{ color: "white" }}>dsds</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
